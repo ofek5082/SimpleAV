06823e43c5b0051a1e7309aefdf478e256020b3f
ofek5082 <46113405+ofek5082@users.noreply.github.com>
2024-08-18 15:54:34 +0300

    Update simple_antivirus.py
    Update so the script uses a config.ini file to retrive settings about scan and certifications

--- simple_antivirus.py
+++ simple_antivirus.py
@@ -1,40 +1,48 @@
-// Importing libraries //
+#// Importing libraries //
   import hashlib 
   import os
+  import configparser 
 
-// load the bad hashes from certification file //
-def load_certification_file(cert_file_path):
-    with open(cert_file_path, 'r') as file:
-      bad_hashes = set(line.strip() for line in file)
+#// load the bad hashes from certification files //
+def load_certification_files(cert_files):
+    bad hashes = set()
+    for cert_file in cert_files
+      with open(cert_file, 'r') as file:
+          bad_hashes.update(line.strip() for line in file)
     return bad_hashes
 
-// Calculate Sha-256 hash of given file //
-def get_file_hash(file_path)
+#// Calculate Sha-256 hash of given file //
+def get_file_hash(file_path):
     hash_algo = hashlib.sha256()
     with open(file_path, 'rb') as file:
         while chunk := file.read(8192):
             hash_algo.update(chunk)
     return hash_algo.hexdigest()
 
-// Scan files in the specified directory for known bad hashes //
-def scan_directory(directory_path, cert_file_path):
-    bad_hashes = load_certification_file(cert_file_path)
+#// Scan files in the specified directory for known bad hashes //
+def scan_directory(directory_path, cert_files):
+    bad_hashes = load_certification_files(cert_files)
     for root, _, files in os.walk(directory_path):
       for file_name in files:
-    file_path = os.path.join(root, file_name)
-    try:
-      file_hash = get_file_hash(file_path)
-      if file_hash in bad_hashes:
-        print(f"Warning: {file_path} contains a known bad signature!")
-    except Exception as e:
-        print(f"Eroor scanning {file_path}: {e}")
+          file_path = os.path.join(root, file_name)
+          try:
+              file_hash = get_file_hash(file_path)
+              if file_hash in bad_hashes:
+               print(f"Warning: {file_path} contains a known bad signature!")
+          except Exception as e:
+              print(f"Eroor scanning {file_path}: {e}")
+            
 
-// Usage Example //
+#// Usage Example //
 if __name__ == "__main__":
-  directory_to_scan = 'path/to/scan' #Replace with the directory you want to scan
-  certification_file = 'certification.txt ' #Replace with your certification file path 
+  config = configparser.configparser()
+  config.read('config.ini') #// Reads config file
 
-scan_directory(direction_to_scan, certification_file)
+  
+  directory_to_scan = config.get('Settings', 'directory_to_scan')
+  certification_file = [file.strip() for file in config.get('Settings', 'certification_files').split(',')] #Replace with your certification file path 
+
+  scan_directory(direction_to_scan, certification_files)
 
 
 ## To run the script use a terminal 
